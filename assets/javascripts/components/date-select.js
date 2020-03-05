3f8fc6da1a3b337b6756ec700ee1cd71cf97b414
AndyRosenberg <andynaterosenberg@gmail.com>
2020-03-04 21:17:28 -0700

    More style foundation and index load

--- assets/javascripts/components/date-select.js
+++ assets/javascripts/components/date-select.js
@@ -10,27 +10,46 @@ Vue.component('complaints-date-select', {
   },
   template: `
     <div>
-      <div class="select is-info is-fullwidth">
+      <div class="select is-dark is-fullwidth">
         <select @change="fetchData($event)">
           <option v-for="item of items" :value="item">{{item}}</option>
         </select>
       </div>
-
-      <ul id="categories">
-        <li v-for="complaint of complaints">
-          <h4>{{complaint.title}}</h4>
-          <p>{{complaint.target}}</p>
-          <pre>{{complaint.body}}</pre>
-        </li>
-      </ul>
+      <section>
+        <div class="hero-body">
+          <div class="container">
+            <section class="section" v-for="complaint of complaints">
+              <div class="columns is-variable is-8">
+                <div class="column is-5 is-offset-1 ">
+                  <div class="content is-medium">
+                    <h2 class="subtitle is-5 has-text-grey">{{complaint.created_at}}</h2>
+                    <h1 class="title has-text-white is-3">{{complaint.title}}</h1>
+                    <h5 class="has-text-grey-light is-5" v-if="complaint.target">
+                      Target: <span class="has-text-grey-lighter">{{complaint.target}}</span>
+                    </h5>
+                  </div>
+                </div>
+                <div class="column is-5">
+                  <div class="content is-medium">
+                    <pre>{{complaint.body}}</pre>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
     </div>
   `,
   methods: {
     fetchData(e) {
-      let value = e.target.value;
-      axios.get('/complaints', { params: { dist: this.dist } }).then(response => {
+      let value = e ? e.target.value : "latest";
+      axios.get('/complaints', { params: { time: value } }).then(response => {
         this.complaints = response.data;
       });
     }
+  },
+  created() {
+    this.fetchData();
   }
 });
